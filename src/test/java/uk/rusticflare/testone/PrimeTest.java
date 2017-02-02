f3272b8c5121245f9f2a8a6e4a6e7f597425447f
James Baker <j_d_baker@hotmail.com>
2017-02-02 23:50:40 +0000

    Soft assertions

--- src/test/java/uk/rusticflare/testone/PrimeTest.java
+++ src/test/java/uk/rusticflare/testone/PrimeTest.java
@@ -1,22 +1,25 @@
 package uk.rusticflare.testone;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static uk.rusticflare.testone.Prime.isPrime;
 
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.junit.Test;
 
 public class PrimeTest {
 
 	@Test
 	public void test() {
-		assertThat(isPrime(-1)).as("-1").isFalse();
-		assertThat(isPrime(0)).as("0").isFalse();
-		assertThat(isPrime(1)).as("1").isFalse();
-		assertThat(isPrime(2)).as("2").isTrue();
-		assertThat(isPrime(3)).as("3").isTrue();
-		assertThat(isPrime(4)).as("4").isFalse();
-		assertThat(isPrime(5)).as("5").isTrue();
-		assertThat(isPrime(6)).as("6").isFalse();
-		assertThat(isPrime(7)).as("7").isTrue();
+		try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+			softly.assertThat(isPrime(-1)).as("-1")
+					.isFalse();
+			softly.assertThat(isPrime(0)).as("0").isFalse();
+			softly.assertThat(isPrime(1)).as("1").isFalse();
+			softly.assertThat(isPrime(2)).as("2").isTrue();
+			softly.assertThat(isPrime(3)).as("3").isTrue();
+			softly.assertThat(isPrime(4)).as("4").isFalse();
+			softly.assertThat(isPrime(5)).as("5").isTrue();
+			softly.assertThat(isPrime(6)).as("6").isFalse();
+			softly.assertThat(isPrime(7)).as("7").isTrue();
+		}
 	}
 }
